5311bed8cc342983d6d7838c1177e8f6ca9df805
Romain Vacheret <romain.vacheret@gmail.com>
2020-12-04 00:53:53 +0100

    feat: add user context

---
+++ src/contexts/UserContext/UserContext.js
@@ -0,0 +1,43 @@
+import React, { createContext, useState } from 'react';
+
+const UserContext = createContext();
+
+const UserContextProvider = (props) => {
+    const [username, setUsername] = useState('');
+    const [lastname, setLastname] = useState('');
+    const [email, setemail] = useState('');
+    const [groups, setGroups] = useState([]);
+    const [friends, setFriends] = useState([]);
+    const [isAuthenticated, setIsAutenticated] = useState(false);
+    
+    const logout = () => {
+        if(!isAuthenticated) {
+            throw new Error('User already logged out');
+        }
+        alert('Tout doux');
+    };
+
+    const login = () => {
+        if(isAuthenticated) {
+            throw new Error('User already logged in');
+        }
+        alert('Tout doux');
+    };
+
+    return (
+        <UserContext.Provider value={{
+            logout,
+            login,
+            username,
+            lastname,
+            email,
+            groups,
+            friends,
+            isAuthenticated
+        }}>
+            { props.children }
+        </UserContext.Provider>
+    );
+};
+
+export default UserContextProvider;
