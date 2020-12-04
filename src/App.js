9d4a87f4618c73de370fd98d96a5e97a6d122b91
Romain Vacheret <romain.vacheret@gmail.com>
2020-12-04 01:05:16 +0100

    feat: add router

--- src/App.js
+++ src/App.js
@@ -2,26 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 import NavBar from 'components/NavBar/NavBar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from 'components/Home/Home';
+
+const groups = [
+  'Un', 'Deux', 'Trois'
+];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
       <NavBar/>
-    </div>
+        <BrowserRouter>
+          <Switch>
+            <Route
+              exact
+              path='/'
+              render={ () => <Home/>}
+            />
+          </Switch>
+        </BrowserRouter>  
+    </> 
   );
 }
 
